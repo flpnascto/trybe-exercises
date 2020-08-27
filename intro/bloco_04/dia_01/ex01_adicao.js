85cb4c7a72a4f7b40696958398e68b31f53537ac
Felipe Nascimento <flpnascto@gmail.com>
2020-08-27 17:06:17 -0300

    Adicionando 05 exercícios de opração aritimética básica.

---
+++ intro/bloco_04/dia_01/ex01_adicao.js
@@ -0,0 +1,7 @@
+let a = 8;
+let b = 4;
+let soma = 0;
+
+soma = a + b;
+
+console.log("Soma a + b =", soma);
---
+++ intro/bloco_04/dia_01/ex05_modulo.js
@@ -0,0 +1,4 @@
+let a = 8;
+let b = 4;
+
+console.log("O resto da divisão de a por b é", a % b);
