404ea7916898c95617c64466d2aff4eff43ba0c6
Felipe Nascimento <flpnascto@gmail.com>
2020-09-21 07:58:03 -0300

    Inserindo função para validação do formulário.

--- intro/bloco_06/dia_01/script.js
+++ intro/bloco_06/dia_01/script.js
@@ -88,9 +88,90 @@ function checkDateFormat() {
   }
 }
 
+function checkTextField(value, character) {
+  let check = false;
+  if (value !== '' && value.length <= character){
+    check = true;
+  }
+  return check;
+}
+
+function checkEmailField(value, character) {
+  let check = false;
+  if (value !== '' && value.length <= character && value.includes('@')){
+    check = true;
+  }
+  return check;
+}
+
+function checkTextField(value) {
+  let check = false;
+  if (value !== ''){
+    check = true;
+  }
+  return check;
+}
+
+function checkRadioButton(elements) {
+  let check = false;
+  for (let element = 0; element < elements.length; element += 1) {
+    if (element.checked) {
+      check = true;
+      break;
+    }
+  }
+  return check;
+}
+
+
+
+function formValidation() {
+  let formValidationMensage = []
+  if (!checkTextField(document.getElementById('nome').value, 40)){
+    formValidationMensage.push('Campo nome de ser preenchido, máximo 40 caracteres');
+  }
+  if (!checkEmailField(document.getElementById('email').value, 50)){
+    formValidationMensage.push('Campo e-mail de ser preenchido, máximo 50 caracteres');
+  }
+  if (!checkTextField(document.getElementById('cpf').value, 11)){
+    formValidationMensage.push('Campo CPF de ser preenchido, máximo 11 caracteres');
+  }
+  if (!checkTextField(document.getElementById('endereco').value, 200)){
+    formValidationMensage.push('Campo endereço de ser preenchido, máximo 200 caracteres');
+  }
+  if (!checkTextField(document.getElementById('cidade').value, 200)){
+    formValidationMensage.push('Campo cidade de ser preenchido, máximo 28 caracteres');
+  }
+  if (!checkTextField(document.getElementById('estados').value, 200)){
+    formValidationMensage.push('Campo estados de ser preenchido');
+  }
+  if (!checkRadioButton(document.querySelectorAll('input[name="tipo-residencia"'))) {
+    formValidationMensage.push('Selecione o tipo de Residência');
+  }
+  if (formValidationMensage.length === 0) {
+    console.log('Formulário ok');
+  } else {
+    const formResultDiv = document.getElementById('form-result');
+    for (let index = 0; index < formValidationMensage.length; index += 1) {
+      formResultDiv.innerHTML += ('- ' + formValidationMensage[index] + '<br>');
+      // formResultDiv.innerHTML += ;
+    }
+  }
+  
+  console.log(formValidationMensage);
+
+}
+
 window.onload = function () {
   gerarOptionEstadosElement();
   
-  const dataInicioInputText = document.querySelector('#data-inicio');
+  const dataInicioInputText = document.getElementById('data-inicio');
   dataInicioInputText.addEventListener('change', checkDateFormat);
+
+  const submitFormButton = document.getElementById('submit-form');
+  submitFormButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    formValidation();
+  });
+  
 }
