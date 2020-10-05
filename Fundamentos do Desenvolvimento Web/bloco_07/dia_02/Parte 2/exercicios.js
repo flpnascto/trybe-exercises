7af175d552fffb6b015b0513739f9eaa291a2387
Felipe Nascimento <flpnascto@gmail.com>
2020-10-05 10:07:46 -0300

    Função que adiciona chave e valor a um objeto.

---
+++ Fundamentos do Desenvolvimento Web/bloco_07/dia_02/Parte 2/exercicios.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// exercicio 01 - Função que adiciona chave e valor a um objeto.
+
+function addTurn(objectToChange, newKey, keyValue) {
+  objectToChange[newKey] = keyValue;
+  return objectToChange;
+}
