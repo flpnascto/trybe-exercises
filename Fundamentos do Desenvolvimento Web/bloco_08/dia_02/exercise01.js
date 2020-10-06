6249eb307bcf17c128cc16e181ead566e55d7d17
Felipe Nascimento <flpnascto@gmail.com>
2020-10-06 19:53:56 -0300

    Exercício 01.

---
+++ Fundamentos do Desenvolvimento Web/bloco_08/dia_02/exercise01.js
@@ -0,0 +1,21 @@
+const books = require('./bookshelf');
+
+const assert = require('assert');
+
+const expected_result = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft'
+];
+
+// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+function formatedBookNames() {
+  return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+}
+
+// console.log(formatedBookNames())
+assert.deepStrictEqual(formatedBookNames(), expected_result);
