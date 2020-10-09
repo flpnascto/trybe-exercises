f537ff6c84734349c6b161907346645f50774570
Felipe Nascimento <flpnascto@gmail.com>
2020-10-08 22:41:39 -0300

    Exercício 06.

--- Fundamentos do Desenvolvimento Web/bloco_08/dia_03/exercise05.js
+++ Fundamentos do Desenvolvimento Web/bloco_08/dia_03/exercise05.js
@@ -14,32 +14,6 @@ const names = [
 
 // Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
 
-// function containsA() {
-//   return names.reduce((sumOfAs, currentName) => {
-//     console.log('Sum ' + sumOfAs + ' Name: ' + currentName)
-//     currentName.split('').forEach((element) => {
-//       console.log(element);
-//       if (element === 'a' || element === 'A') {
-//         sumOfAs += 1;
-//         console.log(sumOfAs);
-//       }
-//       console.log(sumOfAs);
-//       return sumOfAs;
-//     });
-//   }, 0);
-// }
-// function containsA() {
-//   const teste = names.reduce((sumOfAs, currentName) => {
-//     console.log(currentName);
-//     let x = currentName.toLowerCase().split('')
-//     console.log(x)
-//     let y = x.filter((char) => char === 'a');
-//     console.log(y)
-//     // sumOfAs += x.length;
-//   });
-//   return teste;
-// }
-
 function containsA() {
   return names.reduce((sumOfAs, currentName) => {
     sumOfAs += currentName
