31f2060512fab0e2bf54d24ad2003fd1010c7888
Felipe Nascimento <flpnascto@gmail.com>
2020-10-30 19:26:31 -0300

    Exercício 02.

--- Fundamentos do Desenvolvimento Web/bloco_10/dia_03/tests/ex01.test.js
+++ Fundamentos do Desenvolvimento Web/bloco_10/dia_03/tests/ex01.test.js
@@ -1,7 +1,7 @@
 let randomNumbers = require('../exercise01');
 
 describe('Testando a função randomNumbers', () => {
-  test('Se a função foi chamada', () => {
+  test('Se a função foi chamada, quantas vezes e o seu retorno', () => {
     randomNumbers = jest.fn().mockReturnValue(10);
 
     randomNumbers();
@@ -9,4 +9,12 @@ describe('Testando a função randomNumbers', () => {
     expect(randomNumbers).toHaveBeenCalledTimes(1);
     expect(randomNumbers()).toBe(10);
   });
+
+  test('E o seu retorno com os respectivos parâmentros foram passados', () => {
+    randomNumbers = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+    expect(randomNumbers(8, 4)).toBe(2);
+    expect(randomNumbers).toHaveBeenCalledWith(8, 4);
+  })
 })
+
