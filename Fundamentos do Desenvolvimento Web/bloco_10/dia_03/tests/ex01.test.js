1ebcfc495b6d19b2f84a03ee493cbe8c64445899
Felipe Nascimento <flpnascto@gmail.com>
2020-11-10 11:32:59 -0300

    Exercicio 03.

--- Fundamentos do Desenvolvimento Web/bloco_10/dia_03/tests/ex01.test.js
+++ Fundamentos do Desenvolvimento Web/bloco_10/dia_03/tests/ex01.test.js
@@ -8,22 +8,37 @@ describe('Testando a função randomNumbers', () => {
     expect(randomNumbers).toHaveBeenCalled();
     expect(randomNumbers).toHaveBeenCalledTimes(1);
     expect(randomNumbers()).toBe(10);
-    expect(randomNumbers).to
   });
 
-  test('E o seu retorno com os respectivos parâmentros foram passados', () => {
-    randomNumbers = jest.mockImplementation((a, b) => a / b);
+  test('Mockando com implementação (a, b) => a / b', () => {
+    randomNumbers = jest.fn().mockImplementation((a, b) => a / b);
 
     expect(randomNumbers(8, 4)).toBe(2);
+    expect(randomNumbers).toHaveBeenCalled();
+    expect(randomNumbers).toHaveBeenCalledTimes(1);
     expect(randomNumbers).toHaveBeenCalledWith(8, 4);
   })
 
-  test('E o seu retorno com os respectivos parâmentros foram passados', () => {
+  test('Mockando com implementação (a, b, c) => a * b * c', () => {
     randomNumbers = jest.fn().mockImplementation((a, b, c) => a * b * c);
 
     expect(randomNumbers(2, 4, 8)).toBe(64);
-    expect(randomNumbers).toHaveBeenCalledWith(2, 4, 8);
+    expect(randomNumbers).toHaveBeenCalled();
     expect(randomNumbers).toHaveBeenCalledTimes(1);
+    expect(randomNumbers).toHaveBeenCalledWith(2, 4, 8);
+
+  })
+  
+    test('resetando e mockando com implementação (a) => a * 2', () => {
+      randomNumbers.mockReset();
+      expect(randomNumbers).toHaveBeenCalledTimes(0);
+
+      randomNumbers = jest.fn().mockImplementation(a => a * 2);
+  
+      expect(randomNumbers(3)).toBe(6);
+      expect(randomNumbers).toHaveBeenCalled();
+      expect(randomNumbers).toHaveBeenCalledTimes(1);
+      expect(randomNumbers).toHaveBeenCalledWith(3);
   })
 })
 
