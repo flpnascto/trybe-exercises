f9e5f39738ed811e9abaffe24ea726503b0cefbb
Felipe Nascimento <flpnascto@gmail.com>
2020-11-10 18:38:13 -0300

    Criando componente Pokedex.

--- front-end/bloco_11/dia_02/my-pokedex/src/App.js
+++ front-end/bloco_11/dia_02/my-pokedex/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Pokedex from './components/Pokedex';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Pokedex</h1>
       </header>
+      <Pokedex />
     </div>
   );
 }
