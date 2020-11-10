e176ce489c47040033d88e0a75b914dd26419ee9
Felipe Nascimento <flpnascto@gmail.com>
2020-11-10 19:58:54 -0300

    Estilização da aplicação.

--- front-end/bloco_11/dia_02/my-pokedex/src/components/Pokemon.jsx
+++ front-end/bloco_11/dia_02/my-pokedex/src/components/Pokemon.jsx
@@ -5,12 +5,16 @@ class Pokemon extends React.Component {
     const { name, type, averageWeight, image} = this.props.pokemon;
     return (
       <div className="pokemon-card">
-        <div className="poke-name">{name}</div>
-        <div className="poke-type">{type}</div>
-        <div className="poke-weight">
-          {`Average weight ${averageWeight.value} ${averageWeight.measurementUnit}`}
-        </div>
-        <img className="poke-img" alt="" src={image} />
+        <section className="card-section">
+          <div className="poke-name">{name}</div>
+          <div className="poke-type">{type}</div>
+          <div className="poke-weight">
+            {`Average weight ${averageWeight.value} ${averageWeight.measurementUnit}`}
+          </div>
+        </section>
+        <asside className='card-asside'>
+          <img className="poke-img" alt="" src={image} />
+        </asside>
       </div>
     );
   }
