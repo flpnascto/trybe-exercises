af55c1a76ceed51b5140152fce3f9512739c977a
Felipe Nascimento <flpnascto@gmail.com>
2021-01-04 15:46:06 -0300

    Incluindo rotas e componentes Home e Login

--- front-end/bloco_16/dia_03/cadastro-clientes/src/App.js
+++ front-end/bloco_16/dia_03/cadastro-clientes/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Route, Switch } from 'reacto-router';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>Iniciando</h1>
+      <Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
