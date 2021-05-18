cb679a0ba6361dcb230c3820cbdb6316192b4417
Felipe Nascimento <flpnascto@gmail.com>
2021-05-18 20:37:05 -0300

    criar findByPk Books

--- back-end/bloco_31/dia_02/controllers/bookController.js
+++ back-end/bloco_31/dia_02/controllers/bookController.js
@@ -12,4 +12,18 @@ router.get('/books', async (_req, res) => {
   }
 })
 
+router.get('/book/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const books = await Book.findByPk(id);
+
+    if (!books) return res.status(404).json({ message: 'Livro não encontrado' });
+
+    return res.status(200).json(books);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado' })
+  }
+})
+
 module.exports = router;
