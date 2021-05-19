024008bc1c2825404ff9fd5e2d6bad688c570b86
Felipe Nascimento <flpnascto@gmail.com>
2021-05-18 21:04:48 -0300

    criar delete Books

--- back-end/bloco_31/dia_02/controllers/bookController.js
+++ back-end/bloco_31/dia_02/controllers/bookController.js
@@ -43,6 +43,19 @@ router.route('/book/:id')
       console.log(error.message);
       res.status(500).json({ message: 'Algo deu errado' });
     }
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    try {
+      const deleteBook = await Book.destroy({ where: { id } });
+
+      console.log(deleteBook);
+
+      return res.status(200).json({ message: 'Livro removido com sucesso!' });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    }
   });
 
 router.post('/book', async (req, res) => {
