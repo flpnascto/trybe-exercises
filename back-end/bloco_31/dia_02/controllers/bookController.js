898f3dfbf75d1d7c7b7a75012b262bbf1c56c1c9
Felipe Nascimento <flpnascto@gmail.com>
2021-05-18 21:01:35 -0300

    criar update Books

--- back-end/bloco_31/dia_02/controllers/bookController.js
+++ back-end/bloco_31/dia_02/controllers/bookController.js
@@ -12,19 +12,38 @@ router.get('/books', async (_req, res) => {
   }
 });
 
-router.get('/book/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const books = await Book.findByPk(id);
+router.route('/book/:id')
+  .get(async (req, res) => {
+    const { id } = req.params;
+    try {
+      const books = await Book.findByPk(id);
 
-    if (!books) return res.status(404).json({ message: 'Livro não encontrado' });
+      if (!books) return res.status(404).json({ message: 'Livro não encontrado' });
 
-    return res.status(200).json(books);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: 'Algo deu errado' });
-  }
-});
+      return res.status(200).json(books);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    }
+  })
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { title, author, pageQuantity } = req.body;
+    try {
+      const [updateBook] = await Book.update(
+        { title, author, pageQuantity },
+        { where: { id } },
+      );
+
+      console.log(updateBook);
+      if (!updateBook) return res.status(404).json({ message: 'Livro não encontrado' });
+
+      return res.status(200).json({ message: 'Livro atualizado com sucesso!' });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    }
+  });
 
 router.post('/book', async (req, res) => {
   const { title, author, pageQuantity } = req.body;
