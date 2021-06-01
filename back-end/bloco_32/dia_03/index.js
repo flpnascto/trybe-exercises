3dc41b8f66807ab0d81f5b3fdc6358752aaed221
Felipe Nascimento <flpnascto@gmail.com>
2021-06-01 09:49:25 -0300

    configurar socket.io

--- back-end/bloco_32/dia_03/index.js
+++ back-end/bloco_32/dia_03/index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`Cliente conectado. ID: ${socket.id} `);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (_req, res) => {
