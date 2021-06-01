c9ac9b06b8684c23542815ad02a23eb3bea1c063
Felipe Nascimento <flpnascto@gmail.com>
2021-06-01 10:14:04 -0300

    update number of likes

--- back-end/bloco_32/dia_03/public/js/likePost.js
+++ back-end/bloco_32/dia_03/public/js/likePost.js
@@ -3,3 +3,7 @@ const socket = window.io();
 document.querySelector('#likeIcon').addEventListener('click', () => {
   socket.emit('likePost');
 });
+
+socket.on('updateLikes', (currentLikes) => {
+  document.querySelector('#currentLikes').innerHTML = currentLikes;
+})
